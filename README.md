3e364507b8b1fa7c71b21999167a5d43b00c77dd
mansch404 <manuel-asbeck5@t-online.de>
2021-12-14 22:47:15 +0100

    Added Resources

---
+++ README.md
@@ -0,0 +1,4 @@
+# manshOS # 
+### is a simple OS, built completely from scratch. ###
+Here you can see it in its current state of development:
+![screen](res/mansh-os.png)
--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,6 +2,8 @@
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_set_color(PRINT_COLOR_LIGHT_GRAY, PRINT_COLOR_BLACK);
     print_str("Welcome to manshOS!");
+    print_char('\n');
+    print_str("v-beta-0.1");
 }
